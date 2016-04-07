5bfd8be8563421ef92e4dddba8609c7cccee0959
Robin Lovelace <rob00x@gmail.com>
2016-04-07 21:34:11 +0100

    Update work around bug
    https://github.com/ropensci/geojsonio/issues/81

--- analysis/update-cents-scenarios.R
+++ analysis/update-cents-scenarios.R
@@ -53,4 +53,5 @@ head(centsa@data)
 plot(centsa)
 cents_df = centsa@data
 View(cents_df)
-geojson_write(centsa, file = "../pct-bigdata/cents-scenarios.geojson")
+geojson_write(centsa)
+file.copy("myfile.geojson", "../pct-bigdata/cents-scenarios.geojson", overwrite = T)
