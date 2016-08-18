4be51db257d284661e73365d6de87eb37f480541
Robin Lovelace <rob00x@gmail.com>
2016-08-18 09:33:06 +0100

    Update analysis to diagnose issue with cleveland

--- analysis/update-lines-data-remove-NAs.R
+++ analysis/update-lines-data-remove-NAs.R
@@ -9,6 +9,23 @@ missing_lines = read.csv("/tmp/New folder/oneway_delete_lines.csv")
 head(missing_lines) # take a look - just 32 ids
 
 l = readRDS("../pct-bigdata/lines_oneway_shapes_updated.Rds")
+summary("E02002485 E02002517" %in% l$id) # in updated lines? no
+summary("E02002485 E02002517" %in% missing_lines$id) # in missing lines? yes
+
+l_clev = readRDS("../pct-data/cleveland/l.Rds")
+summary("E02002485 E02002517" %in% l_clev$id) # in cleveland new? no
+l_clev = read.csv("https://cdn.rawgit.com/npct/pct-data/46946ee/cleveland/l.csv")
+summary("E02002485 E02002517" %in% l_clev$id) # in cleveland data download? NO.
+
+# solution - update the data sha in pct-shiny :)
+
+# check data sha of pct:
+readLines("../pct-shiny/data_sha")
+# aha - not the latest sha has been downloaded...
+l_clev = read.csv("https://cdn.rawgit.com/npct/pct-data/cf727e4/cleveland/l.csv") # with latest sha
+summary("E02002485 E02002517" %in% l_clev$id) # in cleveland updated data download? yes!
+
+
 nrow(l) # 233478 lines, including with 0 length
 
 sel_missing = l$id %in% missing_lines$id
@@ -27,6 +44,11 @@ summary(sel_na_lines) # 8519 missing
 to_remove = sel_missing | sel_na_lines
 summary(to_remove) # 8551 to remove
 l = l[!to_remove,]
-nrow(l)
+nrow(l) # 224927 lines now
+summary(l$dist)
 
 saveRDS(l, "../pct-bigdata/lines_oneway_shapes_updated.Rds")
+
+old = setwd("../pct-bigdata/")
+system("git commit -am 'Update lines_oneway_shapes_updated.Rds'")
+system("git push origin master")
