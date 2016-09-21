878b336d7fa1909124b31fdf642ec915de2c35c4
audev <au232@medschl.cam.ac.uk>
2016-09-21 10:27:23 +0100

    Update to latest PCT format

--- gm_scenarios/L5_update-cents-scenarios.R
+++ gm_scenarios/L5_update-cents-scenarios.R
@@ -1,67 +1,49 @@
-# Aim: update centroids used in the PCT
+# Aim: create centroids file for PCT
+#This scripts extracts/processes the inflows from pct_lines.dta
 rm(list=ls())
-#THIS SCRIPTS EXTRACTS the inflows from pct_lines.dta
+
 
 # load centroids
-setwd('V:/Group/GitHub/pct')
-source("set-up.R")
+source("../set-up.R")
 
-#list of UK MSOA w.  : geo_code-MSOA-%female-avslope
-centsa = readOGR("../pct-bigdata/cents.geojson", "OGRGeoJSON")
-
+#list of UK MSOA w.  : 7,201 X 84 (MSOA x scenarios values)
+centsa = readOGR("../../pct-bigdata/cents-scenarios.geojson", "OGRGeoJSON")
 head(centsa)
-names(centsa)[names(centsa) == "MSOA11NM"] = "geo_label"
-l_new <- readstata13::read.dta13("//me-filer1/home$/au232/My Documents/1.CEDAR/3_Studies !!/28-DfT2.0/4-Manchester/1-Model OD data DFT2.0/modelODdata~/6-Anna PCT code/160409_PCTforReplication-GM/1b_datacreated/pct_lines_GM.dta")
-l_new <- l_new[, 1:82]
-l_new <- l_new[l_new$all!=0, ]
 
-l_new <- dplyr::rename(l_new,
-                       home_msoa = msoa1,
-                       work_msoa = msoa2,
-                       All = all,
-                       Bicycle = bicycle,
-                       Train = train,
-                       Bus = bus,
-                       Car_driver = car_driver,
-                       Car_passenger = car_passenger,
-                       Foot = foot,
-                       Other = other,
-                       Taxi = taxi,
-                       Motorbike = motorbike,
-                       Other = other
-)
+l_new <- read.csv("./Output/pct_lines_GM.csv", header=T, as.is = T)
+l_new <- l_new[l_new$all!=0, ]
+summary(sel <- l_new$msoa1 == l_new$msoa2)
+l_cents = l_new[sel,]
+rm(l_new)
+
+#renaming cols. to match PCT's
+l_cents <- dplyr::rename(l_cents,
+                       geo_code = msoa1)
 
 # remove excess columns
-l_new$Rail <- l_new$Train + l_new$light_rail
-l_new$Train <- NULL
+l_cents$msoa2 = NULL
+l_cents$rail <- l_cents$train + l_cents$light_rail
+l_cents$train <- NULL
 
-summary(sel <- l_new$Area.of.residence == l_new$Area.of.workplace)
-l_new_cents = l_new[sel,]
-names(l_new)
 
-l_new_cents$Area.of.workplace = NULL
-l_new_cents = rename(l_new_cents, geo_code = Area.of.residence)
+l_cents = inner_join(l_cents, centsa@data[,1:4], by='geo_code')
+l_cents = l_cents[,c(1, 82:84,2:81)]
 
-head(l_new_cents[1:5])
+names(l_cents)
+head(l_cents[1:5])
 head(centsa)
 
 # merge new data into existing data
-summary(centsa$geo_code %in% l_new_cents$geo_code) # no matching variables
-head(centsa$geo_code)
-head(l_new_cents$geo_code)
 centsa$geo_code = as.character(centsa$geo_code)
 head(sort(centsa$geo_code))
-head(sort(l_new_cents$geo_code))
-summary(centsa$geo_code %in% l_new_cents$geo_code)
+head(sort(l_cents$geo_code))
+summary(centsa$geo_code %in% l_cents$geo_code)
 names(centsa)
-l_new_cents$avslope = NULL
 
-#aud: centsa@data = left_join(centsa@data, l_new_cents)
-#centsa@data = inner_join(centsa@data, l_new_cents)
+#aud: make centsa AS l_cents (but what about the spatial bit?)
+#centsa@data=l_cents
 head(centsa@data)
 plot(centsa)
-cents_df = centsa@data
-View(cents_df)
 
 geojson_write(centsa)
 file.copy("myfile.geojson", "../pct-bigdata/cents-scenarios_GM.geojson", overwrite = T)
