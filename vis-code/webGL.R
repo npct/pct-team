40079438e9a21cbbf521fd8b3faf7585ec530c9e
Robin Lovelace <rob00x@gmail.com>
2016-12-16 21:35:28 +0000

    Add new sf-deb based code

--- vis-code/webGL.R
+++ vis-code/webGL.R
@@ -12,6 +12,23 @@ sel_high = rf$all > 100
 plot(rf[sel_high,])
 rf = rf[sel_high,]
 p = spsample(x = rf, n = 5e4, type = "regular", weight = rf$all)
+
+# with new stplanr function:
+# install sfr branch of stplanr and sf
+devtools::install_github(repo = "robinlovelace/stplanr", ref = "sfr-dep")
+devtools::install_github("edzer/sfr")
+library(sf)
+sf::st_line_sample
+system.time(
+  {p = stplanr::line_sample(l = rf, n = 1e5, weights = l$bicycle)}
+)
+plot(p)
+library(dplyr)
+library(leaflet.extras)
+leaflet() %>%
+  addTiles() %>%
+  addWebGLHeatmap(lng = p@coords[,1], lat = p@coords[,2], size = 10, units = "px", alphaRange = 0.0001)
+
 plot(p)
 leaflet() %>%
   addTiles() %>%
